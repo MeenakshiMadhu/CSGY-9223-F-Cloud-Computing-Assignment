2aa6f28dbd30776ea7f221841a4474cfb586ba1b
Meenakshi Madhu <meenakshimadhu00@gmail.com>
2025-10-07 23:50:04 -0400

    updated sdk for acc change

--- frontend/assets/js/sdk/apigClient.js
+++ frontend/assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://90sl5jwf68.execute-api.us-east-1.amazonaws.com/dev';
+    var invokeUrl = 'https://dwshoyi9v2.execute-api.us-east-1.amazonaws.com/dev';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
